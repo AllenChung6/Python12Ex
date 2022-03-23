387fac818aef500d6f9ff73d614d6544e47cadec
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-22 21:52:10 -0400

    Finished gen_list and gen_dict functions.

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -1,3 +1,4 @@
+import math
 from typing import List, Dict, Set, Callable
 import enum
 
@@ -8,6 +9,20 @@ class Parity(enum.Enum):
 
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
+    # normal method
+    #     list1 = []
+    #     for x in range(start, stop):
+    #         if x % 2 == 0 and parity == parity.EVEN:
+    #             list1.append(x)
+    #         elif x % 2 == 1 and parity == parity.ODD:
+    #             list1.append(x)
+    #     return list1
+
+    # List comprehension
+    gen = [x for x in range(start, stop) if
+           (x % 2 == 0 and parity == parity.EVEN) or (x % 2 == 1 and parity == parity.ODD)]
+    return gen
+
     """
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
@@ -18,10 +33,13 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     :param parity:
     :return:
     """
-    pass
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
+    # dictionary comprehension list
+    gen = {x: strategy(x) for x in range(start, stop)}
+    return gen
+
     """
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
